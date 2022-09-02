0abeccaa6c7c83ddfb70e20c8b33bc81bdc67b53
Eliyahu Gamliel <egamliel@g.jct.ac.il>
2022-09-02 17:19:03 +0300

    api

--- src/components/api.js
+++ src/components/api.js
@@ -1,3 +1,3 @@
-const CX = "00cb9c0d772094201";
-const API_KEY = "AIzaSyBA9H0sHDcKdEX9lpJzJ-VD51xPMuergoQ";
+const CX = "00cb9************";
+const API_KEY = "AIzaSyBA9H**********************";
 export { CX, API_KEY };
